1c4bcdbba1f2882ff05d18495df4c0b458fff08c
Hiroki Uchida <nikuyoshi@gmail.com>
2020-05-29 01:57:05 +0900

    Minor adjustment

--- monitoring-1-create-stack.sh
+++ monitoring-1-create-stack.sh
@@ -7,7 +7,7 @@ region=ap-northeast-1
 
 create_stack () {
     echo "Create stack"
-    aws cloudformation create-stack --stack-name monitoring-1 --capabilities CAPABILITY_NAMED_IAM --template-body file://monitoring-1-all-resources.yaml --region $region --profile $profile
+    aws cloudformation create-stack --stack-name monitoring-1 --capabilities CAPABILITY_NAMED_IAM --template-body file://monitoring-1.yaml --region $region --profile $profile
     aws cloudformation wait stack-create-complete --stack-name monitoring-1 --region $region --profile $profile
 }
 
